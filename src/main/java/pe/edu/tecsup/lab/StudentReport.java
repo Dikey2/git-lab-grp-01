1b3f3a3e1fa08129a3feb5dd548a9d480662bd3b
Alexis Villar <alexis.villar@tecsup.edu.pe>
2026-04-01 01:24:09 -0500

    feat(sprint-1): implementación de StudentReport

--- src/main/java/pe/edu/tecsup/lab/StudentReport.java
+++ src/main/java/pe/edu/tecsup/lab/StudentReport.java
@@ -3,6 +3,7 @@ package pe.edu.tecsup.lab;
 public class StudentReport {
 
     public void generateReport() {
-        System.out.println("Reporte generado correctamente");
+        System.out.println("=== REPORTE DE ESTUDIANTES ===");
+        System.out.println("Reporte generado correctamente en sprint-1");
     }
 }
